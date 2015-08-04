6c9becc4fc16145b9a0328026089a2b6fff9710f
Michel Van der Meiren <michelvdmeiren@gmail.com>
2015-08-04 14:46:01 +0200

    vintage theme

--- sys/html_footer.php
+++ sys/html_footer.php
@@ -1,6 +1,8 @@
 </div></main>
 
 <footer><div class="wrap">
+
+
 <?php echo 'Rendered in ', round( microtime(true)-START_TIME , 4 ), ' sec.'; ?>
 </div></footer>
 
@@ -14,7 +16,6 @@
 	<a href="" class="mv-lb-close"><svg><use xlink:href="inc/icons.svg#close-icon">></use></svg></a>
 </span>
 </div>
-
 
 <script src="<?php echo ROOT; ?>/inc/tiny-query.js"></script>
 <script src="<?php echo ROOT; ?>/inc/common.js"></script>
